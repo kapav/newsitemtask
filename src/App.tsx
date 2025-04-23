dad3329175cad869bd362835e21b1413e8b8eb75
Александр Кочкин <cybyn@yandex.ru>
2025-04-23 18:57:20 +0400

    NIT-2_Добавление-NewsItem

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NewsItem from './components/NewsItem'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NewsItem />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,12 @@
+import './NewsItem.scss'
+
+function NewsItem() {
+  return (
+    <>
+      <div>NewsItem works!
+      </div>
+    </>
+  )
+}
+
+export default NewsItem
